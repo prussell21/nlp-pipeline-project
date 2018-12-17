01dcdc49d27d64f713954cadf83432bb6db2e816
Paul Russell <russe1pt@gmail.com>
2018-12-17 02:03:40 -0500

    chore: cleaned up commented code

--- data/process_data.py
+++ data/process_data.py
@@ -4,33 +4,33 @@ from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    
+
     '''
     Loads category and messsage data
-    
+
     INPUTS
     categories: dataset containing classification of messages
     messages: dataset of actual text of messages
-    
+
     OUPUT
     Merged dataframe of messages and their corresponding classified categories
     '''
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
-    
+
     df = messages.merge(categories, left_on='id', right_on='id')
-    
+
     return df
 
 
 def clean_data(df):
-    
+
     '''
     Cleans dataset for model training
-    
+
     INPUT
     df: dataset of merged messages and their categories
-    
+
     OUPTU
     df: cleaned dataset
     '''
@@ -38,43 +38,36 @@ def clean_data(df):
     row = categories.iloc[0]
     category_colnames = row.apply(lambda x: str(x)[:-2])
     categories.columns = category_colnames
-    
+
     for column in categories:
-        
+
         # set each value to be the last character of the string
         categories[column] = categories[column].apply(lambda x: str(x)[-1])
-    
+
         # convert column from string to numeric
         categories[column] = categories[column].astype(int)
-        
+
     # drop the original categories column from `df`
     df = df.drop('categories', axis=1)
-        
+
     # concatenate the original dataframe with the new `categories` dataframe
     frames = [df, categories]
     df = pd.concat(frames, axis=1)
-    
+
     # check number of duplicates
     df.duplicated(keep='first').sum()
-    
+
     # drop duplicates
     df = df.drop_duplicates()
-    
-    #Creates dummy variables for 'genre' category
-#     genre_table = pd.get_dummies(df['genre'])
-#     genre_table = genre_table.astype(int)
-#     df = pd.concat([df, genre_table], axis=1)
-    
-    #Drops original 'genre' column
-#     df = df.drop('genre', axis=1)
-    
+
+
     return df
-    
+
 def save_data(df, database_filename):
-    
+
     '''
     Saves dataset to SQlite database
-    
+
     INPUT
     df: dataset of merged categories and messages
     database_filename: filepath for desired storage of SQlite table
@@ -94,12 +87,12 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print (len(sys.argv))
         print('Please provide the filepaths of the messages and categories '\
